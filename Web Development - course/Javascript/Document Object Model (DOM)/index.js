aca8baf21509541b33829916f172c8a5b9814776
Loghic <michalek.matej.loghin@gmail.com>
2024-10-15 02:39:27 +0200

    DOM

--- Web Development - course/Javascript/Document Object Model (DOM)/index.js
+++ Web Development - course/Javascript/Document Object Model (DOM)/index.js
@@ -1,1 +1,6 @@
 document.querySelector("ul").lastElementChild.innerHTML="hello";
+document.querySelector("h1").classList.toggle("huge");
+
+document.querySelector("a").attributes;
+document.querySelector("a").getAttribute("href");
+document.querySelector("a").setAttribute("href", "https://bing.com");
